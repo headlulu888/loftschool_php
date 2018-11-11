32b58de3f47a50bbe467be65ab4920f7cd499bdb
headlulu888 <headlulu888@gmail.com>
2018-11-12 00:23:12 +0300

    task0_1

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrey-PC
+ * Date: 12.11.2018
+ * Time: 0:20
+ */
+
+CONST testConstanta = 'asd';
+$user_name = "Igor";
+
+function show_something() {
+    return 'something';
+}
+
+if(1 == 1) {
+    echo 'hi';
+}
